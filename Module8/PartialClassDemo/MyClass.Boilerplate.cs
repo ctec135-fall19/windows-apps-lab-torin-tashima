4ba497808639c4e9e33bd70c41a938a9fb21c205
Torin Tashima <t.tashima@students.clark.edu>
2019-11-14 14:54:19 -0800

    Windows App Lab Submission 11/14/19

---
+++ Module8/PartialClassDemo/MyClass.Boilerplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassDemo
+{
+    partial class MyClass
+    {
+        private int myField;
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Module8/PartialClassDemo/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassDemo
+{
+    partial class MyClass
+    {
+        public MyClass(int x)
+        {
+            this.myField = x;
+        }
+
+        public void printState()
+        {
+            Console.WriteLine("state: {0}", myField);
+        }
+    }
+}
